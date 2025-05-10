a030d55f7657b84a1f2a7125e3841db06183af27
juathanduarte <juathan.cduarte@inf.ufpel.edu.br>
2025-05-10 00:03:05 -0300

    feature: criando alias para importações dinâmicas

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,19 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
+	resolve: {
+		alias: {
+			"@": "/src",
+			"@components": "/src/components",
+			"@pages": "/src/pages",
+			"@assets": "/src/assets",
+			"@hooks": "/src/hooks",
+			"@contexts": "/src/contexts",
+			"@services": "/src/services",
+			"@utils": "/src/utils",
+			"@models": "/src/models",
+			"@enums": "/src/enums",
+			"@helpers": "/src/helpers",
+		},
+	},
 });
